a929202540dd3bdba322ca1c066fdab9fb8d18d8
ranggo pato <patoranggo@gmail.com>
2024-07-07 21:40:50 +0700

    loading

--- src/app/products/template.tsx
+++ src/app/products/template.tsx
@@ -4,6 +4,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
 export default function RootLayout({
 	children,
@@ -11,8 +12,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	const pathname = usePathname();
+	const [search, setSearch] = useState('')
 	return (
 		<>
+		<input type="text" value={search} onChange={e => setSearch(e.target.value)} />
 			<ul className='flex text-sm text-blue-300 gap-4'>
 				<Link
 					className={pathname === '/products/food' ? 'text-red-200' : ''}
